8c40ef1615f8d013f21dbc0ee443baa0962085e7
Guy Ni <guy@spectrocloud.com>
2026-04-07 14:30:20 -0700

    Changed ingressController.Spec.EndpointPublishingStrategy to NodePort for MAAS

--- control-plane-operator/hostedclusterconfigoperator/controllers/resources/ingress/reconcile.go
+++ control-plane-operator/hostedclusterconfigoperator/controllers/resources/ingress/reconcile.go
@@ -34,7 +34,8 @@ func ReconcileDefaultIngressController(ingressController *operatorv1.IngressCont
 		ingressController.Spec.DefaultCertificate = &corev1.LocalObjectReference{
 			Name: manifests.IngressDefaultIngressControllerCert().Name,
 		}
-	case hyperv1.KubevirtPlatform:
+	case hyperv1.KubevirtPlatform, hyperv1.MAASPlatform:
+		// KubeVirt and MAAS: no cloud LoadBalancer for the default router; NodePort matches ingress-operator expectations.
 		ingressController.Spec.EndpointPublishingStrategy = &operatorv1.EndpointPublishingStrategy{
 			Type: operatorv1.NodePortServiceStrategyType,
 		}
@@ -93,14 +94,6 @@ func ReconcileDefaultIngressController(ingressController *operatorv1.IngressCont
 					},
 				},
 			},
-		}
-		ingressController.Spec.DefaultCertificate = &corev1.LocalObjectReference{
-			Name: manifests.IngressDefaultIngressControllerCert().Name,
-		}
-	case hyperv1.MAASPlatform:
-		// MAAS platform uses default LoadBalancer strategy
-		ingressController.Spec.EndpointPublishingStrategy = &operatorv1.EndpointPublishingStrategy{
-			Type: operatorv1.LoadBalancerServiceStrategyType,
 		}
 		ingressController.Spec.DefaultCertificate = &corev1.LocalObjectReference{
 			Name: manifests.IngressDefaultIngressControllerCert().Name,
